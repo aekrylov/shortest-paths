c8698bad15c95c6e4e0c0e5664519e8c29f239d3
Constantine <cmen.itis@gmail.com>
2017-05-16 05:43:03 +0300

    the night calculations added

--- topic42/main.py
+++ topic42/main.py
@@ -2,7 +2,7 @@ from topic42.graph import Graph
 from topic42.algos import dijkstra_heap, floyd_warshall, ford_bellman
 import time
 
-orders = [i for i in range(100, 2001, 100)]
+orders = [i for i in range(100, 30001, 100)]
 
 dijkstra_results = open('results/dijkstra.txt', 'w')
 floyd_warshall_results = open('results/floyd_warshall.txt', 'w')
@@ -17,11 +17,11 @@ for order in orders:
         dijkstra_heap(graph.nodes, graph.nodes[i])
     dijkstra_results.write(str(order) + ' ' + str(time.clock() - time_before) + '\n')
 
-    # time_before = time.clock()
-    # for i in range(order):
-    #     ford_bellman(graph.nodes, graph.nodes[i])
-    # ford_bellman_results.write(str(order) + ' ' + str(time.clock() - time_before) + '\n')
+    time_before = time.clock()
+    for i in range(order):
+        ford_bellman(graph.nodes, graph.nodes[i])
+    ford_bellman_results.write(str(order) + ' ' + str(time.clock() - time_before) + '\n')
 
-    # time_before = time.clock()
-    # floyd_warshall(graph.nodes)
-    # floyd_warshall_results.write(str(order) + ' ' + str(time.clock() - time_before) + '\n')
+    time_before = time.clock()
+    floyd_warshall(graph.nodes)
+    floyd_warshall_results.write(str(order) + ' ' + str(time.clock() - time_before) + '\n')
